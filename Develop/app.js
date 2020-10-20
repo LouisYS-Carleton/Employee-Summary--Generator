be44b883bff707c82275ea79dd9c6f28643eb5af
Louis Yacksmith <louisyscarleton@gmail.com>
2020-10-20 15:12:06 -0400

    add to app.js, moved DS_STORE

--- Develop/app.js
+++ Develop/app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const htmlRenderer = require('./lib/htmlRenderer');
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,6 +10,143 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+const managerQuestions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: "Please enter the managers name."
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Please enter the managers ID number.'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter the managers email address.'
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: "Please enter the managers office number."
+    },
+    {
+      type: 'confirm',
+      name: 'addMoreWorkers',
+      message: 'Would you like to add another worker?',
+      default: false
+    }
+]
+
+const engineerQuestions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: "Please enter the engineers name."
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Please enter the engineers ID number.'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter the engineers email address.'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: "Please enter the engineers github username."
+    },
+    {
+      type: 'confirm',
+      name: 'addMoreWorkers',
+      message: 'Would you like to add another worker?',
+      default: false
+    }
+]
+
+const internQuestions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: "Please enter the interns name."
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Please enter the interns ID number.'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter the interns email address.'
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: "Please enter the interns school."
+    },
+    {
+      type: 'confirm',
+      name: 'addMoreWorkers',
+      message: 'Would you like to add another worker?',
+      default: false
+    }
+]
+
+const employeeTypeQuestion = [
+    {   
+        type: 'list',
+        name: 'employeeTypeQuestion',
+        message: 'What type of employee would you like to add?',
+        choice: [
+            'Manager',
+            'Engineer',
+            'Intern'
+        ]
+    }
+]
+
+
+// function writeToFile(fileName, data) {
+// fs.writeFile(fileName, data, function (err) {
+//   if (err) {
+//     return console.log(err)
+//   }
+//   console.log("README has been successfully completed.")
+// })
+// }
+
+function init() {
+    inquirer
+    .prompt(employeeTypeQuestion)
+    .then((answer) => {
+        if (answer.choice === 'Manager') {
+            inquirer
+            .prompt(managerQuestions)
+            .then((managerAnswers) => {
+                let newManager = new Manager(answerManager.name, answerManager.email, answerManager.id, answerManager.officeNumber);
+                
+            })
+        } else if (answer.choice === 'Intern') {
+            inquirer
+            .prompt(internQuestions)
+            .then((internAnswers) => {
+                let newIntern = new Manager(internAnswers.name, internAnswers.email, internAnswers.id, internAnswers.school);
+            })
+        } else if (answer.choice === 'Engineer') {
+            inquirer
+            .prompt(engineerQuestions)
+            .then((engineerAnswers) => {
+                let newEngineer = new Engineer(engineerAnswers.name, engineerAnswers.email, engineerAnswers.id, engineerAnswers.github);
+            })
+        }
+    })
+}
 
 
 // Write code to use inquirer to gather information about the development team members,
