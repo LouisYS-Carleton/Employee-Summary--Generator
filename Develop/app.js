9892f9b8d9097d29225a14d58c299747abe2659f
Louis Yacksmith <louisyscarleton@gmail.com>
2020-10-21 16:22:58 -0400

    Get extra employee question to show, at wrong time though.

--- Develop/app.js
+++ Develop/app.js
@@ -10,51 +10,64 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-let employees = [];
+// let employees = [];
 
 const employeeQuestions = () => {
     inquirer
         .prompt([
             {
-                type: 'list',
-                name: 'role',
-                message: 'Select an Employee type you wish to enter:',
-                choices: [
-                'Manager',
-                'Engineer',
-                'Intern'
-                ]
+              type: 'list',
+              name: 'role',
+              message: 'Please select an employee type you wish to enter:',
+              choices: [
+              'Manager',
+              'Engineer',
+              'Intern'
+              ]
             },
             {
-                type: 'input',
-                name: 'name',
-                message: 'What is this employee\'s name?'
+              type: 'input',
+              name: 'name',
+              message: "Please enter the employee's name."
             },
             {
-                type: 'input',
-                name: 'id',
-                message: 'What is this employee\'s id?'
+              type: 'input',
+              name: 'id',
+              message: "Please enter the employee's id."
             },
             {
-                type: 'input',
-                name: 'email',
-                message: 'What is this employee\'s email?'
+              type: 'input',
+              name: 'email',
+              message: "Please enter the employee's email."
             },
         ])
         .then(answers => {
             const { role } = answers;
-        switch(role) {
-            case 'Manager':
-                specificEmployeeQuestions(role, "officeNumber", "What is the Manager's office number?", answers);
-            break;
-            case 'Engineer':
-                specificEmployeeQuestions(role, "github", "What is the Engineer's Github profile username?", answers);
-            break;
-            case 'Intern':
-                specificEmployeeQuestions(role, "school", "Where does the Intern go to school?", answers);
-            break;
-        }
-    });
+            switch(role) {
+              case 'Manager':
+                  specificEmployeeQuestions(role, "officeNumber", "Please enter the managers office number.", answers);
+              break;
+              case 'Engineer':
+                  specificEmployeeQuestions(role, "github", "Please enter the ngineer's Github profile username.", answers);
+              break;
+              case 'Intern':
+                  specificEmployeeQuestions(role, "school", "Please enter the name of the school that the intern attended.", answers);
+              break
+            }
+        });
 }
 
-employeeQuestions();
+const addAnotherEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'confirm',
+        name: 'additionalEmployeesPrompt',
+        message: 'Would you like to add another employee?'
+      }
+  ]);
+}
+
+// function specificEmployeeQuestions(role, answers){ console.log('bklhasdkhad') };
+employeeQuestions()
+addAnotherEmployee()
