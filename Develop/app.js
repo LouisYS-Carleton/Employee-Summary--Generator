7fef092f9946bb21ea73059ebfc06ba55f4fb8f1
Louis Yacksmith <louisyscarleton@gmail.com>
2020-10-21 17:28:27 -0400

    added generic output writeFile function, tidying code.

--- Develop/app.js
+++ Develop/app.js
@@ -13,48 +13,48 @@ const render = require("./lib/htmlRenderer");
 // let employees = [];
 
 const employeeQuestions = () => {
-    inquirer
-        .prompt([
-            {
-              type: 'list',
-              name: 'role',
-              message: 'Please select an employee type you wish to enter:',
-              choices: [
-              'Manager',
-              'Engineer',
-              'Intern'
-              ]
-            },
-            {
-              type: 'input',
-              name: 'name',
-              message: "Please enter the employee's name."
-            },
-            {
-              type: 'input',
-              name: 'id',
-              message: "Please enter the employee's id."
-            },
-            {
-              type: 'input',
-              name: 'email',
-              message: "Please enter the employee's email."
-            },
-        ])
-        .then(answers => {
-            const { role } = answers;
-            switch(role) {
-              case 'Manager':
-                  specificEmployeeQuestions(role, "officeNumber", "Please enter the managers office number.", answers);
-              break;
-              case 'Engineer':
-                  specificEmployeeQuestions(role, "github", "Please enter the ngineer's Github profile username.", answers);
-              break;
-              case 'Intern':
-                  specificEmployeeQuestions(role, "school", "Please enter the name of the school that the intern attended.", answers);
-              break
-            }
-        });
+  inquirer
+    .prompt([
+        {
+          type: 'list',
+          name: 'role',
+          message: 'Please select an employee type you wish to enter:',
+          choices: [
+          'Manager',
+          'Engineer',
+          'Intern'
+          ]
+        },
+        {
+          type: 'input',
+          name: 'name',
+          message: "Please enter the employee's name."
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: "Please enter the employee's id."
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: "Please enter the employee's email."
+        },
+    ])
+    .then(answers => {
+        const { role } = answers;
+        switch(role) {
+          case 'Manager':
+              specificEmployeeQuestions(role, "officeNumber", "Please enter the managers office number.", answers);
+          break;
+          case 'Engineer':
+              specificEmployeeQuestions(role, "github", "Please enter the ngineer's Github profile username.", answers);
+          break;
+          case 'Intern':
+              specificEmployeeQuestions(role, "school", "Please enter the name of the school that the intern attended.", answers);
+          break
+        }
+    });
 }
 
 const addAnotherEmployee = () => {
@@ -63,10 +63,27 @@ const addAnotherEmployee = () => {
       {
         type: 'confirm',
         name: 'additionalEmployeesPrompt',
-        message: 'Would you like to add another employee?'
+        message: 'Would you like to add another employee?',
+        default: false
       }
-  ]);
+    ])
+    .then(answers => {
+      if (answers.additionalEmployeesPrompt === true) {
+
+      } else {
+
+      }
+    })
 }
+
+const copyEmployeeInfoToHTML = (html) => {
+  fs.writeFile(outputPath, html, function(err) {
+    if (err) { 
+      return console.log(err); 
+    }
+    console.log("Employee Summary HTML File was successfully created.");   
+  });
+};
 
 // function specificEmployeeQuestions(role, answers){ console.log('bklhasdkhad') };
 employeeQuestions()
